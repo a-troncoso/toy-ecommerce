d19448b74a6bfbf6ae13eab2d2bba5f5b88a9890
Alvaro Troncoso <alvaro.troncoso@overactive.com>
2022-02-02 21:27:39 -0300

    clean store

--- store/reducers.js
+++ store/reducers.js
@@ -1,46 +1,10 @@
 import { combineReducers } from "redux";
-import * as types from "./types";
 
 import productReducer from "./product/productReducer";
 import categoryReducer from "./category/categoryReducer";
 import cartReducer from "./cart/cartReducer";
 
-const counterReducer = (state = 0, { type }) => {
-  switch (type) {
-    case types.INCREMENT:
-      return state + 1;
-    case types.DECREMENT:
-      return state - 1;
-    case types.RESET:
-      return 0;
-    default:
-      return state;
-  }
-};
-
-// INITIAL TIMER STATE
-const initialTimerState = {
-  lastUpdate: 0,
-  light: false,
-};
-
-// TIMER REDUCER
-const timerReducer = (state = initialTimerState, { type, payload }) => {
-  switch (type) {
-    case types.TICK:
-      return {
-        lastUpdate: payload.ts,
-        light: !!payload.light,
-      };
-    default:
-      return state;
-  }
-};
-
-// COMBINED REDUCERS
 const reducers = {
-  counter: counterReducer,
-  timer: timerReducer,
   product: productReducer,
   category: categoryReducer,
   cart: cartReducer,
